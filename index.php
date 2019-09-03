315fc24e16d235fbfa09c9d402875678fdb1f36f
Takashi <program.takashi@gmail.com>
2019-09-03 16:56:16 +0900

    [start] make php file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Image Uploader</title>
+    <style>
+      body{
+        text-align: center;
+        font-family: Arial, sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+    <form action="" method="post" enctype="multipart/form-data">
+
+    </form>
+  </body>
+</html>
